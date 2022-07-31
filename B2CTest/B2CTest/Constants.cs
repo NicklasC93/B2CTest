75993cce511ba371c9e5823a23dfdbd541451d0f
padellisten <padellisten@gmail.com>
2022-07-31 15:12:08 +0200

    Testing..

---
+++ B2CTest/B2CTest/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B2CTest
+{
+    public static class Constants
+    {
+        public static readonly string TenantName = "padeldrengene";
+        public static readonly string TenantId = "padeldrengene.onmicrosoft.com";
+        public static readonly string ClientId = "cdc54b78-8859-489c-a570-54d675218578";
+        public static readonly string SignInPolicy = "B2C_1_signupsignin1";
+        public static readonly string IosKeychainSecurityGroups = "com.<yourcompany>.<groupname>"; // e.g com.contoso.aadb2cauthentication
+        public static readonly string[] Scopes = new string[] { "openid", "offline_access" };
+        public static readonly string AuthorityBase = $"https://{TenantName}.b2clogin.com/tfp/{TenantId}/";
+        public static readonly string AuthoritySignIn = $"{AuthorityBase}{SignInPolicy}";
+    }
+}
---
+++ B2CTest/B2CTest/LoginPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Identity.Client;
+using Xamarin.Forms;
+
+namespace B2CTest
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            try
+            {
+                // Look for existing account
+                var accounts = await App.AuthenticationClient.GetAccountsAsync();
+
+                if (accounts.Count() >= 1)
+                {
+                    AuthenticationResult result = await App.AuthenticationClient
+                        .AcquireTokenSilent(Constants.Scopes, accounts.FirstOrDefault())
+                        .ExecuteAsync();
+
+                    await Navigation.PushAsync(new LoginResultPage(result));
+                }
+            }
+            catch
+            {
+                // Do nothing - the user isn't logged in
+            }
+            base.OnAppearing();
+        }
+
+        async void OnSignInClicked(object sender, EventArgs e)
+        {
+            AuthenticationResult result;
+
+            try
+            {
+                result = await App.AuthenticationClient
+                    .AcquireTokenInteractive(Constants.Scopes)
+                    .WithPrompt(Prompt.ForceLogin)
+                    .WithParentActivityOrWindow(App.UIParent)
+                    .ExecuteAsync();
+
+                await Navigation.PushAsync(new LoginResultPage(result));
+            }
+            catch (MsalClientException)
+            {
+
+            }
+        }
+    }
+}
